13f8b46adc33735001dcd553b4659f075c006570
Michele M <mnmin@gmail.com>
2022-08-24 16:39:25 +0100

    pets post

--- src/routers/pets.js
+++ src/routers/pets.js
@@ -22,4 +22,25 @@ router.get('/', async (req, res) => {
     })
 })
 
+router.post('/', async (req, res) => {
+
+    let sqlQuery = 'INSERT INTO pets (name, age, type, breed, microchip) VALUES ($1, $2, $3, $4, $5) RETURNING *;'
+
+    const queryParams = [
+
+        req.body.name,
+        Number(req.body.age),
+        req.body.type,
+        req.body.breed,
+        Boolean(req.body.microchip)
+
+    ]
+
+    const qResult = await db.query(sqlQuery, queryParams)
+
+    res.status(201).json({
+        pets: qResult.rows[0]
+    })
+})
+
 module.exports = router
